040206f2df9995f25a4515cd2597558ad6fce17a
User058 <110759720+User058@users.noreply.github.com>
2024-05-25 00:26:03 +0300

    Update ssh-vpn.sh
    Signed-off-by: User058 <110759720+User058@users.noreply.github.com>

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -281,11 +281,11 @@ socket = l:TCP_NODELAY=1
 socket = r:TCP_NODELAY=1
 
 [dropbear]
-accept =  9000
+accept =  445
 connect = 127.0.0.1:109
 
 [openssh]
-accept = 900
+accept = 777
 connect = 127.0.0.1:443
 
 [openvpn]
