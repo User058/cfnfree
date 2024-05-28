ea303d88a9809ba66940b86ebb4a7dc79a20576a
User058 <110759720+User058@users.noreply.github.com>
2024-05-28 12:35:36 +0300

    Update m-dns.sh
    Signed-off-by: User058 <110759720+User058@users.noreply.github.com>

--- websocket/m-dns.sh
+++ websocket/m-dns.sh
@@ -14,8 +14,8 @@ clear
 echo -e "\e[36m╒════════════════════════════════════════════╕\033[0m"
 echo -e " \E[0;47;30m                 DNS CHANGER                \E[0m"
 echo -e "\e[36m╘════════════════════════════════════════════╛\033[0m
-\033[1;37mDNS Changer By NevermoreSSH\033[0m
-\033[1;37mTelegram : https://t.me/todfix667 \033[0m"
+\033[1;37mDNS Changer By User058\033[0m
+\033[1;37mTelegram : https://t.me/user058 \033[0m"
 dnsfile="/root/dns"
 if test -f "$dnsfile"; then
 udns=$(cat /root/dns)
